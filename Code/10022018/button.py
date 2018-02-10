15cb51bf28506c7e140f6a1e77186cfc48a80981
Jerome-h <Jerome-h@users.noreply.github.com>
2018-02-10 14:52:54 +0000

    New Day New Code

---
+++ Code/10022018/button.py
@@ -0,0 +1,13 @@
+from machine import Pin
+import time
+
+button = Pin(16, Pin.IN)
+
+while True:
+    if button.value() == 0:
+    	print ("Button is not pressed")
+    	time.sleep(0.3)
+    
+    else:
+    	print("Button has been pressed")
+    	time.sleep(0.3)
